1d513fbffe1c59b1d980ca1dd1afb577e4fffcd8
Harlan Wilton <harlan@harlanzw.com>
2021-02-15 15:21:14 +1100

    chore(tests): adding vue-bootstrap testing fixture

---
+++ test/fixtures/bootstrap/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <b-nav>
+      <b-nav-item active>
+        Active
+      </b-nav-item>
+      <b-nav-item>Link</b-nav-item>
+      <b-nav-item>Another Link</b-nav-item>
+      <b-nav-item disabled>
+        Disabled
+      </b-nav-item>
+    </b-nav>
+    <div class="text-2xl">
+      <h1 data-testid="hello" class="bg-blue-700 font-bold text-white px-5 py-5">
+        Hello World
+      </h1>
+    </div>
+  </div>
+</template>
